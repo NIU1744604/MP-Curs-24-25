0809a20f6a007329285379a14a8034f380cafe25
NIU1744604 <1744604@uab.cat>
2025-02-17 20:34:54 +0100

    Update cerca_substitueix.cpp

--- tema_0/exercicis_avaluables/exercici_1/source/cerca_substitueix.cpp
+++ tema_0/exercicis_avaluables/exercici_1/source/cerca_substitueix.cpp
@@ -4,15 +4,72 @@
 
 bool cercaString(char text[], char subStr[], int &posicio)
 {
+ int i = posicio; //comença amb la posicio desitjada
+bool trobat = false;
+
+int longstr = strlen(str);
+int longTEXT = strlen(text);
+int j = 0;
+
+while ((!trobat) && (text[i] != '\0')) {
+
+	while (text[i + k] == str[i]) {
+		if (i == longstr) {
+			trobat = true;
+			posicio = i;
+		}
+		else {
+			j++;
+		}
+	}
+}
+
+return trobat; 
 
 }
 
 void substitueixString(char text[], int posicio, char stringOriginal[], char nouString[])
 {
+//saber les long. de les cadenes: 
+int longTEXT = strlen(text);
+int longOriginal = strlen(stringOriginal);
+int longNOU = strlen(nouString);
+int diferencia;
 
+if (longNOU > longOriginal) {
+
+	diferencia = longNOU - longOriginal;
+
+	for (int i = longTEXT; i >= posicio + longOriginal; i--) {
+		text[i + diferencia] = text[i];
+	}
+}
+
+if (longNOU < longOriginal) {
+
+	diferencia = longOriginal - longNOU;
+	for (int i = posicio + longOriginal; i <= longTEXT; i++) {
+		text[i + diferencia] = text[i];
+		//moure a l'esquerra per omplir els espais buits. 
+	}
+}
+
+for (int i = 0; i < longNOU; i++) {
+	text[posicio + i] = nouString[i];
+	//posisio + i, posicio indica per on començar, i la i, per anar avançant posicions en el string. 
+}
 }
 
 int cercaSubstitueix(char text[], char stringOriginal[], char nouString[])
 {
+ int numsub = 0;
+int posicio = 0;
+
+while (cercaString(text, stringOriginal, posicio)) {
+	substitueixString(text, posicio, stringOriginal, nouString);
+	numsub++;
+	posicio = posicio + strlen(nouString);
+}
+return numsub; 
 
 }
